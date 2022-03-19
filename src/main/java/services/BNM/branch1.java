3c329ffb9347adddcbc5dff3efc24a731ceb5593
admin <petrov.alexandru21@mail.ru>
2022-03-19 13:55:37 +0200

    Initial commit

---
+++ src/main/java/services/BNM/branch1.java
@@ -0,0 +1,15 @@
+package services.BNM;
+
+import java.io.*;
+
+class branch11 {
+    public static void main(String[] args)
+    {
+
+
+        System.out.println("new file");
+
+
+        System.out.println("edit file");
+    }
+}
